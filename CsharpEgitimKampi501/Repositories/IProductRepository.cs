44e83ed107ad07e945d5ba5d1b694c5f403c8034
htemel <htemel@DESKTOP-1QU3AO4>
2025-03-19 22:15:27 +0300

    Add project files.

---
+++ CsharpEgitimKampi501/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsharpEgitimKampi501.Dtos;
+
+namespace CsharpEgitimKampi501.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<ResultProductDto>> GetAllProductsAsync();
+        Task CreateProductAsyn(CreateProductDto createProductDto);
+        Task UpdateProductAsync(UpdateProductDto updateProductDto);
+        Task DeleteProductAsync(int id);
+        Task GetByProductIdAsync(int id);
+    }
+}
---
+++ CsharpEgitimKampi501/Repositories/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsharpEgitimKampi501.Dtos;
+
+namespace CsharpEgitimKampi501.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task CreateProductAsyn(CreateProductDto createProductDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProductAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultProductDto>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetByProductIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProductAsync(UpdateProductDto updateProductDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
